42f022c2ba6cfb273b3e1a93cc6b65932d4851d4
Ilich Arredondo <ilichh1@hotmail.com>
2020-01-13 20:46:29 -0600

    Codigo del dia 5

---
+++ day-5/server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const carros = [
+  {
+    id: 55,
+    made: 'Subaru'
+  }
+];
+
+app.use(bodyParser.json());
+
+app.get('/pruebas', (req, res) => {
+  console.log(req.headers);
+  res.send();
+});
+
+app.get('/consultar', (req, res) => {
+  const { id: idToFind } = req.query;
+  console.log('el usuario quiere buscar ', idToFind);
+  if (!!idToFind) {
+    const carroEncontrado = carros.find(({ id }) => id == idToFind);
+    console.log(carroEncontrado);
+    if (!carroEncontrado) {
+      res.send({
+        msg: 'No hay carros con el id: ' + idToFind
+      });
+      return;
+    }
+    res.send({
+      msg: 'Tiene un resultado',
+      carroEncontrado
+    });
+    return;
+  }
+  res.send({
+    msg: 'has consultado informacion',
+    carros
+  });
+});
+
+app.get('/consultar/:id', (req, res) => {
+  const carroToUpdate = carros.find(({ id }) => id == req.params.id);
+  res.send({
+    msg: 'has consultado informacion',
+    carroToUpdate
+  });
+});
+
+app.post('/guardar', (req, res) => {
+  carros.push(req.body);
+  res.send({
+    msg: 'Has guardado informacions',
+    savedId: req.body.id
+  });
+});
+
+app.put('/actualizar/:idCarro', (req, res) => {
+  const carroToUpdate = carros.find(({ id }) => id == req.params.idCarro);
+  const indexToUpdate = carros.indexOf(carroToUpdate);
+  console.log(carroToUpdate);
+  carros[indexToUpdate] = {
+    ...carros[indexToUpdate],
+    ...req.body
+  };
+  res.send({
+    msg: 'Has actualizados informacions',
+    carroActualizado: carros[indexToUpdate]
+  });
+});
+
+app.delete('/borrar/:idCarro', (req, res) => {
+  const carroToUpdate = carros.find(({ id }) => id == req.params.idCarro);
+  const indexToUpdate = carros.indexOf(carroToUpdate);
+  delete carros[indexToUpdate];
+  res.status(204);
+  res.send();
+});
+
+app.listen(8080, () => console.log('El servidor esta vivo'));
